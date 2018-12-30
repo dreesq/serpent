a8d511974f2067c71d3bdda5b79a4e08f564bd4f
bthe0 <badescu.theodor@gmail.com>
2018-12-30 13:24:52 +0200

    Autofilter model utility.

--- examples/api/actions/tasks.js
+++ examples/api/actions/tasks.js
@@ -1,33 +1,22 @@
-const {config} = require('../../../index');
+const {config, utils} = require('../../../index');
 
 config({
     name: 'getTasks',
     input: {
-        page: 'number'
+        filters: 'object'
     },
     middleware: [
         'auth'
     ]
 })(
-    /**
-     * A test action
-     * @returns {Promise<{success: boolean}>}
-     */
-
-    async ({db, user, input}) => {
-        const limit = 5;
-        const skip = (input.page - 1 || 0) * limit;
-
-        const get = db.Task.find({userId: user._id}).skip(skip).limit(limit);
-        const count = db.Task.count();
-
-        const [data, total] = await Promise.all([get, count]);
-
-        return {
-            data,
-            total
-        };
-    }
+    utils.autoFilter('Task', {
+        restrictToUser: true,
+        pagination: true,
+        limit: 2,
+        transform(data) {
+            return data;
+        }
+    })
 );
 
 config({
