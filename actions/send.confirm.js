3be117365da35de57b83b9551e26c78c46c14072
bthe0 <badescu.theodor@gmail.com>
2019-01-29 19:31:01 +0200

    Added resend confirmation action.

---
+++ actions/send.confirm.js
@@ -0,0 +1,51 @@
+const {config, getPlugins} = require('../index');
+const {config: configPlugin} = getPlugins();
+const {TOKEN_TYPE_CONFIRM, USER_STATUS_INACTIVE} = require('../constants');
+const {error, makeToken} = require('../utils');
+
+config({
+    name: 'sendConfirm',
+    middleware: [
+        'auth'
+    ],
+    enabled: configPlugin.get('plugins.auth.confirm'),
+})(
+    /**
+     * Resend user activation code
+     * @param db
+     * @param mail
+     * @param user
+     * @param i18n
+     * @returns {Promise<void>}
+     */
+
+    async ({db, mail, user, i18n}) => {
+        const t = i18n.translate;
+
+        if (user.status !== USER_STATUS_INACTIVE) {
+            return error(t('errors.alreadyConfirmed'));
+        }
+
+        const token = await makeToken();
+
+        await db.Token.findOneAndUpdate({
+            userId: user._id,
+            type: TOKEN_TYPE_CONFIRM
+        }, {
+            $set: {
+                token
+            }
+        });
+
+        await mail({
+            to: user.email,
+            subject: t('emails.confirmAccount.subject'),
+            html: t('emails.confirmAccount.html', {
+                user: user.toObject(),
+                url: utils.url('confirm', {token})
+            })
+        });
+
+        return success();
+    }
+);
