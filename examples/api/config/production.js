f55b5d9d669293588fc87556309bc1c8f3394ea5
Theodor Badescu <theo@btheo.com>
2019-05-11 17:14:54 +0300

    Use deepmerge for handling config and translations.

--- examples/api/config/production.js
+++ examples/api/config/production.js
@@ -6,23 +6,7 @@ module.exports = () => {
   return {
     debug: false,
     server: {
-      port: 3000,
-      helmet: true,
-      parsers: true,
-      ssl: {
-        cert: './ssl/cert',
-        key: './ssl/key'
-      },
-      cors: {
-        origin: '*',
-        credentials: true
-      },
-      session: {
-        enabled: true,
-        resave: false,
-        saveUninitialized: false,
-        secret: 'mySecret'
-      }
+      port: 3001
     }
   };
 };
--- examples/api/index.js
+++ examples/api/index.js
@@ -1,7 +1,7 @@
 const {setup, start} = require('../../index');
 const app = require('express')();
 
-//process.env.NODE_ENV = 'production';
+process.env.NODE_ENV = 'production';
 
 (async () => {
     await setup(app, {
